f01eb9ed2577f5b036ecc50fcfdaab574e7fb54a
Odpluskwiacz <michal.bralski@tutanota.com>
2026-04-13 00:35:50 +0200

    GameOver Prep gitignore fix

--- main.c
+++ main.c
@@ -1,45 +1,53 @@
 #include "raylib.h"
 
-// TODO BOX ENEMY further development
-// TODO SCORE = GAME TIME 
-// TODO BOX ENEMY SPEED == SCORE HIGHT 
+#define screenWidth 800
+#define screenHeight 450
+#define PlayerSize 50
+#define Gravity 4000
 
+// TODO GAME OVER
+
+Vector2 BoxSize = { 50, 50 };
+Vector2 Player_one_Position = { (float)screenWidth / 5, 300};
+Vector2 BoxPosition = {  500, 300};
+Vector2 Podloga = { 0,  (float)screenHeight / 1.5 + PlayerSize };
+Vector2 RozmiarPodloga = { screenWidth, 100};
+bool OnGround = true;
+float Velocity;
+bool GamePause = false;
+
+
+
+
+void GameOver(double Score){
+  // STOP PLAYER
+  // STOP BOX 
+  // STOP SCORE 
+ DrawText(TextFormat("Score: %.0f", Score), screenWidth/2, screenHeight/2, 50, BLACK);
+ GamePause = true;
+};
+
+void StartGame(){
+  // RESET PLAYER
+  // RESET BOX
+  // RESET SCORE
+}
 
 
 int main(void)
-{
-  const int screenWidth = 800;
-  const int screenHeight = 450;
-  const int PlayerSize = 50; 
-
-  Vector2 BoxSize = { 50, 50 };
-  
-  
-
-  Vector2 Player_one_Position = { (float)screenWidth / 5, 300};
-  Vector2 BoxPosition = {  500, 300};
-
+{  
   InitWindow(screenWidth, screenHeight, "Endles Runner mutliplayer");
   SetTargetFPS(60);
-
-  Vector2 Ziemia = { 0,  (float)screenHeight / 1.5 + PlayerSize };
-  Vector2 Rozmiar = { screenWidth, 100};
-
-  bool OnGround = true;
-  float Gravity = 4000;
-  float Velocity;
 
   // MAIN GAME LOOP
   while (!WindowShouldClose()){
   //========================== LOGIKA ====================
-
-
   //========================== LOGIKA - SKOK ============
-  if (IsKeyPressed(KEY_SPACE) && OnGround ){
+  if (IsKeyDown(KEY_SPACE) && OnGround ){
     Velocity = -1400;
     OnGround = false;
   }
-  //Opadanie
+  //========================== LOGIKA - Opadanie ========
   if(!OnGround) {
     Velocity += Gravity * GetFrameTime(); // Grawitacja ciągnąca w dół
     Player_one_Position.y += Velocity * GetFrameTime(); //Ruch na podstawie prędkości
@@ -49,23 +57,23 @@ int main(void)
     Velocity = 0;
     OnGround = true;
   }
+  //========================= LOGIKA - SCORE COUNT
+  double Score = GetTime();
   //======================== LOGIKA - ENEMY BOX
-  BoxPosition.x -= 2;
+  BoxPosition.x -= 2 + Score/2;
   if(BoxPosition.x < 0 - BoxSize.x){
     BoxPosition.x = screenWidth;
   }
 
-  //========================= LOGIKA - SCORE COUNT
-  double Score = GetTime();
 
   //========================= LOGIKA - COLISION BOX
   bool IsCollision;
   if(CheckCollisionPointCircle(BoxPosition, Player_one_Position, PlayerSize )){
     IsCollision = true;
+    GameOver(Score);
   } else {
     IsCollision = false;
   }
-
 
 
 
@@ -77,7 +85,7 @@ int main(void)
     DrawCircleV(Player_one_Position, PlayerSize, MAROON);
     DrawRectangleV( BoxPosition, BoxSize , BLACK);
     
-    DrawRectangleV(Ziemia, Rozmiar, DARKGRAY );
+    DrawRectangleV(Podloga, RozmiarPodloga, DARKGRAY );
 
     DrawText(TextFormat("Y Pos: %.0f", Player_one_Position.y), 10, 10, 20, BLACK);
     DrawText(TextFormat("Velocity: %.02f", Velocity), 10, 40, 20, BLACK);
@@ -88,7 +96,6 @@ int main(void)
 
   EndDrawing();
   }
-  
   CloseWindow();
   return 0;
 }
